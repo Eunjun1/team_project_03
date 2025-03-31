ea8fc8706b20b0fe9eb3d2d986e7384bc145c4f5
whatslung <wisung92@gmail.com>
2025-03-31 16:16:50 +0900

    성배 완성

--- no092/android/app/src/main/kotlin/com/example/projectpkm/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.projectpkm
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- projectpkm/lib/main.dart
+++ projectpkm/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:projectpkm/no443/pkmon443.dart';
-import 'package:projectpkm/no443/pkmon444.dart';
-import 'package:projectpkm/no443/pkmon445.dart';
-import 'package:projectpkm/showhome.dart';
+import 'package:projectpkm/home.dart';
+import 'package:projectpkm/pkmon092.dart';
+import 'package:projectpkm/pkmon093.dart';
+import 'package:projectpkm/pkmon094.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,31 +14,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pokemon',
+      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      initialRoute: '/showhome',
+      initialRoute: '/no092',
 
       routes: {
-        '/showhome': (context) => Showhome(),
+        
+        '/main': (context) => Home(),
+        // '/showhome': (context)=>Showhome(),
 
         // '/no' : (context) => , // 1tab
         // '/no' : (context) => ,
         // '/no' : (context) => ,
-        // '/no' : (context) => , // 2tab
-        // '/no' : (context) => ,
-        // '/no' : (context) => ,
+        '/no092' : (context) => Pkmno092(), // 2tab
+        '/no093' : (context) => Pkmno093(),
+        '/no094' : (context) => Pkmno094(),
         // '/no' : (context) => , // 3tab
         // '/no' : (context) => ,
         // '/no' : (context) => ,
-        '/no443': (context) => Pkmon443(), // 4tab
-        '/no444': (context) => Pkmon444(),
-        '/no445': (context) => Pkmon445(),
+        // '/no' : (context) => , // 4tab
+        // '/no' : (context) => ,
+        // '/no' : (context) => ,
         // '/no' : (context) => , // 5tab
         // '/no' : (context) => ,
         // '/no' : (context) => ,
+
+
+
+
+
+      
+      
+      
+      
       },
+
+
+
+
     );
   }
 }
