b3b5bd6048c0b1ed431d98cced67069818683669
whatslung <wisung92@gmail.com>
2025-03-31 16:54:34 +0900

    성배 완완!

--- projectpkm/lib/no092/pkmno092main.dart
+++ projectpkm/lib/no092/pkmno092main.dart
@@ -14,20 +14,14 @@ class _Pkmno092mainState extends State<Pkmno092main> {
       body: Center(
         child: Column(
           children: [
-            Image.asset(
-              'images/092.png',
-              width: 300,
-            ),
+            Image.asset('images/092.png', width: 300),
             Text("고오스"),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/no092');
-              }, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red
-              ),
-              child: Text("도감보기",
-              style: TextStyle(color: Colors.white),),
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: Text("도감보기", style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
