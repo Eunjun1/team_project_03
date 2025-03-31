ed5ad95a5cf1ab184febc392a604c6a299e16073
runpon <rlaxoals990504@gmail.com>
2025-03-31 21:42:53 +0900

    파티클 이펙트 추가

--- projectpkm/lib/pkmno396main.dart
+++ projectpkm/lib/pkmno396main.dart
@@ -1,4 +1,11 @@
+import 'package:fade_out_particle/fade_out_particle.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_gesture_detector/simple_gesture_detector.dart';
+
+// 추가 패키지
+//simple_gesture_detector: ^0.2.1
+//fade_out_particle: ^1.2.2
+
 
 class Pkmno396main extends StatefulWidget {
   const Pkmno396main({super.key});
@@ -7,45 +14,85 @@ class Pkmno396main extends StatefulWidget {
   State<Pkmno396main> createState() => _MyWidgetState();
 }
 
+late bool disApper;
+
 class _MyWidgetState extends State<Pkmno396main> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red,
-      
+  void initState() {
+    super.initState();
+    disApper = false;
+  }
 
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-        child: Container(
-          color: Colors.black,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    final double screenWidth = screenSize.width;
+    final double screenHeight = screenSize.height;
+    return Scaffold(
+
+      body: SimpleGestureDetector(
+        onTap: () => _onTap(),
+        child: Column(
+          children: [
+            Stack(
               children: [
-                Image.asset('images/396.webp',
-                width: 300,
-                height: 300,),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Text('찌르꼬',
-                  style: TextStyle(color: Colors.white,
-                  fontSize: 40),),
+                Positioned(
+                  child: Container(
+                    width: screenWidth,
+                    height: screenHeight,
+                    color: Colors.black,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('images/396.webp', width: 300, height: 300),
+                          Text(
+                            '찌르꼬',
+                            style: TextStyle(color: Colors.white, fontSize: 40),
+                          ),
+                          ElevatedButton(
+                            onPressed:
+                                () => Navigator.pushNamed(context, '/no396'),
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(0),
+                              ),
+                              backgroundColor: Colors.red,
+                            ),
+                            child: Text(
+                              '도감열기',
+                              style: TextStyle(fontSize: 40, color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () => Navigator.pushNamed(context, '/no396'),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
-                    backgroundColor: Colors.red
-                  ), 
-                  child: Text('도감열기',
-                  style: TextStyle(fontSize: 40,
-                  color: Colors.white),)
-                )
+                Positioned(
+                  right: 56.5,
+                  top : 200,
+                  child: Container(
+                    child: FadeOutParticle(
+                      disappear: disApper,
+                      child: Image.asset('images/card_back.png'),
+                    ),
+                  ),
+                ),
               ],
             ),
-          ),
+          ],
         ),
       ),
     );
+  }
+
+  _onTap() {
+    if (disApper == false) {
+      disApper = true;
+    } else {
+      disApper = false;
+    }
+    setState(() {});
   }
 }
